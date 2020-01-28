d4cf063635d11960c697852d8f05812a9c80067d
jeantetj <joey.jeantet@gmail.com>
2020-01-28 09:54:24 +0100

    correction de bug mineur souligner par codeFactor

--- WHPServer/src/serveur/loader.java
+++ WHPServer/src/serveur/loader.java
@@ -9,7 +9,9 @@ class loader extends Thread {
         while (showProgress) {
             System.out.print("\r["+anim.charAt(x++ % anim.length())+"] Generating Key ");
             try { Thread.sleep(400); }
-            catch (Exception e) {};
+            catch (Exception e) {
+                System.err.println(e);
+            };
         }
         System.out.print("\r\u001B[32m[Ok] Generating Key ");
         System.out.println("\n\u001B[32mKey Generated");
